cd9a250ac18930d5e4bf1b5a41de15bd31d4014b
Thaigo Bianeck <thgbianeck@gmail.com>
2024-07-16 23:28:28 -0300

    Correção

--- src/infrastructure/express.ts
+++ src/infrastructure/express.ts
@@ -1,14 +1,14 @@
 import express, { Express } from 'express';
 import { Sequelize } from 'sequelize-typescript';
 import { ClientModel } from '../modules/client-adm/repository/client.model';
-import { ProductModel } from '../modules/product-adm/repository/product.model';
-import { clientRoute } from './api/routes/client.route';
-import { productRoute } from './api/routes/product.route';
-import { invoiceRoute } from './api/routes/invoice.route';
-import { checkoutRoute } from './api/routes/checkou.route';
-import TransactionModel from '../modules/payment/repository/transaction.model';
 import { InvoiceModel } from '../modules/invoice/repository/invoice.model';
 import { InvoiceItemModel } from '../modules/invoice/repository/item.model';
+import TransactionModel from '../modules/payment/repository/transaction.model';
+import { ProductModel } from '../modules/product-adm/repository/product.model';
+import { checkoutRoute } from './api/routes/checkout.route';
+import { clientRoute } from './api/routes/client.route';
+import { invoiceRoute } from './api/routes/invoice.route';
+import { productRoute } from './api/routes/product.route';
 
 export const app: Express = express();
 app.use(express.json());
